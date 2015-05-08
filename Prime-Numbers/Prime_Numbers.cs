7becdcfb3fcca66d6c87651ed107b2df5faeb2ef
rshah77 <raj77shah@hotmail.co.uk>
2015-05-08 17:01:44 +0100

    Create Prime_Numbers.cs
    CHALLENGE DESCRIPTION:
    
    Print out the prime numbers less than a given number N. For bonus points your solution should run in N*(log(N)) time or better. You may assume that N is always a positive integer.
    
    INPUT SAMPLE:
    
    Your program should accept as its first argument a path to a filename. Each line in this file is one test case. Each test case will contain an integer n < 4,294,967,295. E.g.
    
    10
    20
    100
    
    OUTPUT SAMPLE:
    
    For each line of input, print out the prime numbers less than N, in ascending order, comma delimited. (There should not be any spaces between the comma and numbers) E.g.
    
    2,3,5,7
    2,3,5,7,11,13,17,19
    2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97"

---
+++ Prime-Numbers/Prime_Numbers.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            PrimeNumbers(uint.Parse(line));
+        }
+    }
+
+    public static void PrimeNumbers(uint number)
+    {
+        List<int> primeNumberList = new List<int>();
+
+        for (uint i = 2; i < number; i++)
+        {
+            if (IsPrime(i))
+            {
+                primeNumberList.Add(int.Parse(i.ToString()));
+            }
+        }
+        System.Console.WriteLine(String.Join(",", primeNumberList));
+    }
+
+    public static bool IsPrime(uint n)
+    {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        int m = (int)System.Math.Sqrt(n);
+        for (int i = 3; i <= m; i += 2)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
